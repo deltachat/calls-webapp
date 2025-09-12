3b15b34cfd83100838f0b6bd96aff75331582deb
adbenitez <asieldbenitez@gmail.com>
2025-09-12 14:21:15 +0200

    update src/vite-env.d.ts with new api

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,8 @@ declare global {
       startCall: (payload: string) => void;
       acceptCall: (payload: string) => void;
       endCall: () => void;
+      getIceServers: () => string;
+      getAvatar: () => string;
     };
   }
 }
